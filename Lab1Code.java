562eba809f751c78cfee123b29740e603c6d43b6
Gigi <gli@loyola.edu>
2024-01-24 13:17:32 -0500

    created code with comments and algorithm file

---
+++ Lab1Code.java
@@ -0,0 +1,15 @@
+// Lab1Code.java
+// Programmer: Gigi Li and Jorge Sacasa
+// Course:  CS 212, Mr. John
+// Due Date: 1/28/24
+// In Class Assessment:
+// Problem Statement:
+// Inputs:
+// Output:
+// Credits:
+
+import java.util.Scanner;
+
+public class Lab1Code {
+
+}
